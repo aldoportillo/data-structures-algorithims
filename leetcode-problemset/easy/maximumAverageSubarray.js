c403758f86d4b5aadffd36c9fd977b68c261cb9f
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-01-16 00:07:47 -0600

    Solve maximum average subarray

---
+++ leetcode-problemset/easy/maximumAverageSubarray.js
@@ -0,0 +1,47 @@
+// Maximum Average Subarray I
+
+// Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average value.
+// And you need to output the maximum average value.
+
+// Example 1:
+// Input: [1,12,-5,-6,50,3], k = 4
+// Output: 12.75
+
+// Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
+
+// Note:
+// 1 <= k <= n <= 30,000.
+// Elements of the given array will be in the range [-10,000, 10,000].
+
+
+var findMaxAverage = function(nums, k) {
+    
+    let largest = -Infinity
+
+    for (let leftPointer = 0; leftPointer < nums.length; leftPointer++){
+
+        let average = 0
+        let rightPointer = leftPointer + k - 1
+
+        if (rightPointer >= nums.length) return largest;
+        
+        while(leftPointer <= rightPointer){
+            average += nums[rightPointer]
+            rightPointer--
+        }
+
+        average /= k
+
+        if (average > largest){
+            largest = average
+        }
+
+    }
+
+    return largest
+
+
+};
+
+console.log(findMaxAverage([1,12,-5,-6,50,3], 4))
+console.log(findMaxAverage([5], 1))
