d89da6adf5d07661d982819afceba2fbb66c3670
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2025-01-24 14:20:15 -0600

    Finish problem

--- medium/jump-game-3/main.rb
+++ medium/jump-game-3/main.rb
@@ -1,3 +1,39 @@
 require "debug"
+require "set"
 
-pp "hello world"
+def jump_game_3(arr, start_index)
+
+    stack = [start_index]
+    visited = Set.new
+
+    while stack.length > 0
+
+        i = stack.pop
+        pp "Index: #{i}. Current Stack: #{stack.to_a()}"
+        jump = arr[i]
+        pp "Jump length #{jump}"
+
+        if jump === 0 
+            return true
+        end
+
+        if !visited.include?(i)
+            visited.add(i)
+            if i + jump < arr.length
+                stack.push(i + jump)
+                pp "Pushing #{i + jump} to stack: #{stack}"
+            end
+
+            if i - jump >= 0 
+                stack.push(i - jump)
+                pp "Pushing #{i - jump} to stack: #{stack}"
+            end
+        end
+
+        pp "#{visited.to_a()}"
+    end
+
+    return false
+end
+
+pp jump_game_3([4, 2, 3, 0, 3, 1, 2], 7)
